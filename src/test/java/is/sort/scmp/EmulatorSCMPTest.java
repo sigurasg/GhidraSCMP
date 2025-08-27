3b7cb33b015badde8719c833cb257426e0dd50ca
Sigurður Ásgeirsson <siggi@sort.is>
2025-08-27 07:44:01 -0400

    Fix wraparound in EA, PTR and disassembly. (#6)
    Fix the language spec to compute EA and PTR with the specified paging behavior.
    Add some tests for same.

--- src/test/java/is/sort/scmp/EmulatorSCMPTest.java
+++ src/test/java/is/sort/scmp/EmulatorSCMPTest.java
@@ -43,4 +43,95 @@ public class EmulatorSCMPTest extends AbstractEmulatorTest {
 		assertEquals(getP3(), 0x0030);
 		assertEquals(getPC(), 0X0001);
 	}
+
+	@Test
+	public void LD_PCRel() {
+		int PC = 0x8100;
+		write(PC, 0xC0, 0x10);
+		write(0x8112, 0xFF);
+		stepFrom(PC);
+		assertEquals(0xFF, getAC());
+	}
+
+	@Test
+	public void JMP_PCRel() {
+		int PC = 0x8100;
+		// PC-relative JMP.
+		write(PC, 0x90, 0x10, 0x90, 0xFF);
+		stepFrom(PC);
+		assertEquals(getPC(), PC + 0x12);
+
+		PC += 2;
+		stepFrom(PC);
+		assertEquals(getPC(), PC + 0x01);
+	}
+
+	@Test
+	public void JMP_P1Rel() {
+		// P1-relative JMP.
+		setP1(0x0200);
+		write(0x0100, 0x91, 0x12);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0212);
+	}
+
+	@Test
+	public void JP() {
+		write(0x100, 0x94, 0x10);
+
+		// Test zero.
+		setAC(0x00);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0112);
+
+		// Test positive
+		setAC(0x7F);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0112);
+
+		// Test negative.
+		setAC(0x81);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0102);
+	}
+
+	@Test
+	public void JZ() {
+		write(0x100, 0x98, 0x10);
+
+		// Test zero.
+		setAC(0x00);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0112);
+
+		// Test positive
+		setAC(0x7F);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0102);
+
+		// Test negative.
+		setAC(0x81);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0102);
+	}
+
+	@Test
+	public void JNZ() {
+		write(0x100, 0x9C, 0x10);
+
+		// Test zero.
+		setAC(0x00);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0102);
+
+		// Test positive
+		setAC(0x7F);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0112);
+
+		// Test negative.
+		setAC(0x81);
+		stepFrom(0x0100);
+		assertEquals(getPC(), 0x0112);
+	}
 }
