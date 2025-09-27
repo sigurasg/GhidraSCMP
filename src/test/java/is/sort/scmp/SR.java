ee795b9da0557fae623ce5ba5030bebf8eaa9591
Sigurdur Asgeirsson <siggi@sort.is>
2025-09-27 13:43:21 +0000

    Rename CC->SR, implement and use in emulator tests.

--- src/test/java/is/sort/scmp/SR.java
+++ src/test/java/is/sort/scmp/SR.java
@@ -14,11 +14,13 @@
 
 package is.sort.scmp;
 
-public class CC {
-	public static final int C = 0x01;
-	public static final int V = 0x02;
-	public static final int Z = 0x04;
-	public static final int N = 0x08;
-	public static final int I = 0x10;
-	public static final int H = 0x20;
+public class SR {
+  public static final int CYL = 0x80; // CY/L: Carry/Link
+  public static final int OV = 0x40;  // OV: Overflow
+  public static final int SB = 0x20;  // SB: Sense Bit B
+  public static final int SA = 0x10;  // SA: Sense Bit A`
+  public static final int IE = 0x08;  // IE: Interrupt Enable
+  public static final int F2 = 0x04;  // F2: User Flag 2
+  public static final int F1 = 0x02;  // F1: User Flag 1
+  public static final int F0 = 0x01;  // F0: User Flag 0
 }
